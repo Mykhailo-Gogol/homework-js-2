8adcf11073eaec5e3f0614b281020459a1fd1c7d
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-11-19 12:07:42 +0200

    Додав homework-2-3

---
+++ homework-2-3.js
@@ -0,0 +1,20 @@
+function findLongestWord(string = "") {
+  // Write code under this line
+  let arr = string.split(" ");
+  let longestWord = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (longestWord.length < arr[i].length) {
+      longestWord = arr[i];
+    }
+  }
+  return longestWord;
+}
+
+//console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+//console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+//console.log(findLongestWord('May the force be with you'));
+// 'force'
